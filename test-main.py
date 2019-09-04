dad64dacd57283b16622ba4a3b514c38eda921e1
moonwave <moonwave.systems@gmail.com>
2019-09-03 23:33:09 -0700

    added requirements.txt

---
+++ test-main.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas
+import openpyxl
+import sys
+
+def test_main():
+    print("enter test_main()")
+    print("exit test_main()")
+    exit(0)
+    exit(1)
+    exit("error message")
+    sys.exit(0)
+    sys.exit(1)
+    sys.exit("error message")
+
+
+if __name__ == "__main__":
+    test_main()
+
